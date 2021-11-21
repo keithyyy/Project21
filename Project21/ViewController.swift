7a715afaebab97e0a255a752f82d4cec68170960
Keith Crooc <keith.crooc@gmail.com>
2021-11-21 14:31:14 -0500

    Completing the tutorial for project21 where we dive into Notifications. The process of asking for permission, then configuring the kinds of information and actions to be displayed with the user's notification center

--- Project21/ViewController.swift
+++ Project21/ViewController.swift
@@ -6,14 +6,109 @@
 //
 
 import UIKit
+import UserNotifications
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(schedulelocal))
+        
     }
 
+    
+    @objc func registerLocal() {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .badge, .sound]) {
+            granted, error in
+            if granted {
+                print("yay")
+            } else {
+                print("doh")
+            }
+        }
+    }
+    
+    @objc func schedulelocal() {
+        registerCategories()
+        
+        let center = UNUserNotificationCenter.current()
+//        creating an instance of our notification center
+        
+//        we want to cancel all pending notifications (in this example, would be notifications that are waiting to be delivered and are waiting for the trigger to send out.
+        center.removeAllPendingNotificationRequests()
+        
+
+        
+        
+//        configuring what kind of notification and how it's like
+        let content = UNMutableNotificationContent()
+        content.title = "Late wake up call"
+        content.body = "The early bird catches the word, but the second mouse gets the cheese."
+        
+        content.categoryIdentifier = "alarm"
+//        this is the type of notification we show
+        content.userInfo = ["customData": "fizzbuzz"]
+        
+        content.sound = .default
+//        with this you can of course customize it to be your own sound.
+        
+        
+        
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 10
+        dateComponents.minute = 30
+        
+        
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        //        interval trigger > calendar trigger. (wait 5 seconds, THEN show this thing) in some cases
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
+        
+    }
+    
+    func registerCategories() {
+        let center = UNUserNotificationCenter.current()
+        
+        center.delegate = self
+        
+        let show = UNNotificationAction(identifier: "show", title: "Tell Me More", options: .foreground)
+//        .foreground means when this button is tapped, launch app immediately
+        
+        let category = UNNotificationCategory(identifier: "alarm", actions: [show], intentIdentifiers: [])
+        
+        center.setNotificationCategories([category])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        
+        if let customData = userInfo["customData"] as? String {
+            print("custom data received: \(customData)")
+            
+            
+            switch response.actionIdentifier {
+            case UNNotificationDefaultActionIdentifier:
+                print("Default Identifier")
+            case "show":
+                print("show more info")
+            default:
+                break
+            }
+            
+            
+        }
+        
+        completionHandler()
+    }
 
 }
 
